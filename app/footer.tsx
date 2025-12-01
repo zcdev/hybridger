1486543e61b970dd238631c8dc465de40ef2ecfc
zcdev <zcdev@hotmail.com>
2025-11-30 20:39:28 -0800

    refactor(layout): extract footer and header components and consolidate for layout

---
+++ app/footer.tsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+
+    return (
+        <footer className="bg-accent-1 border-t border-accent-2">
+            <div className="container mx-auto px-5">
+                <div className="py-28 flex flex-col lg:flex-row items-center">
+                    <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+                        Built with Next.js + Contentful.
+                    </h3>
+                    <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
+                        This website is a conceptual prototype for personal demo purposes and it does not represent a real business or provide actual services.
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
