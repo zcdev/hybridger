7e86b8ad4e6d6ad0d3b0f6963dd7c021453910b3
zcdev <zcdev@hotmail.com>
2025-12-08 14:53:21 -0800

    style(ui): prevent iOS touch device zoom by increasing input font size to 17px

--- app/components/chatbot/chat-input.tsx
+++ app/components/chatbot/chat-input.tsx
@@ -3,12 +3,12 @@ import { ChatInputProps } from "@/lib/types";
 export default function ChatInput({ onSubmit, input, setInput }: ChatInputProps) {
 
     return (
-        <form onSubmit={(e) => onSubmit(e)} className="p-3">
+        <form onSubmit={(e) => onSubmit(e)} className="p-1">
             <input
                 name="chat-input"
                 value={input}
                 onChange={(e) => setInput(e.target.value)}
-                className="w-full rounded-md bg-neutral-200 dark:bg-neutral-800 p-2 text-sm"
+                className="w-full rounded-md bg-neutral-200 dark:bg-neutral-800 p-1 text-[17px]"
                 placeholder="Type something…"
             />
         </form>
