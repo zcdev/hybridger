80089014c94540c6d322fee4de35654bda37345f
zcdev <zcdev@hotmail.com>
2025-12-10 17:21:54 -0800

    style(ui): address Safari browser quirk for space inconsistency - enforce chatbot button and paragraph to display as block - increase top margin space on paragraph

--- app/components/chatbot/chatbot.tsx
+++ app/components/chatbot/chatbot.tsx
@@ -14,11 +14,11 @@ export default function Chatbot() {
                 <div className="chatbot mb-2 md:mb-0 mr-3 md:mr-0">
                     <button
                         onClick={() => setOpen(true)}
-                        className="rounded-full bg-indigo-600 text-white shadow-lg w-[70px] h-[70px]"
+                        className="block rounded-full bg-indigo-600 text-white shadow-lg w-[70px] h-[70px]"
                     >
                         <ImageWrapper src="https://images.ctfassets.net/nxqkiyet73hh/4uYmMHR9hFtGEkd2z74OBn/74df222ff6bfa20260f9b81a2f62e046/hybridger-chatbot.webp" alt="Chatbot" width={50} height={50} />
                     </button>
-                    <p className="text-sm text-center text-indigo-600 font-semibold">Chat now</p>
+                    <p className="block pt-1 text-sm text-center text-indigo-600 font-semibold">Chat now</p>
                 </div>
             )}
         </div>
