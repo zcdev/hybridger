ab591d99e158ade3968b9778b0ae459ca746fcd7
zcdev <zcdev@hotmail.com>
2026-03-23 11:45:38 -0700

    style(logo): logo and favicon refresh 2026

--- app/components/ui/logo.tsx
+++ app/components/ui/logo.tsx
@@ -1,24 +1,24 @@
 import ContentfulImage from "@/lib/contentful-image";
 
-const lightModeLogo = "https://images.ctfassets.net/nxqkiyet73hh/mhI5JId9K4gvmef3Wzfxy/b8cc7b11a8c06f1d5dd012ecbc53add6/hybrider-logo-light.svg";
+const lightModeLogo = "https://images.ctfassets.net/nxqkiyet73hh/mhI5JId9K4gvmef3Wzfxy/734ac34e10d2ae4ca2162bc12aaa1100/hybridger-logo-light.svg";
 
-const darkModeLogo = "https://images.ctfassets.net/nxqkiyet73hh/AExFYv0mOmAKciFO14IB3/e3f28ff028b430d16e29d3a504812f26/hybrider-logo-dark.svg";
+const darkModeLogo = "https://images.ctfassets.net/nxqkiyet73hh/AExFYv0mOmAKciFO14IB3/0486ff2afa83f54987b9cc2a2e13ad4d/hybridger-logo-dark.svg";
 
 export default function Logo() {
     return (
         <>
             <ContentfulImage
-                className="logo inline-flex align-[-13px] dark:hidden"
+                className="logo inline-flex align-[-9px] dark:hidden"
                 alt="Hybridger logo"
-                width={50}
-                height={77}
+                width={93}
+                height={60}
                 src={lightModeLogo}
             />
             <ContentfulImage
-                className="logo hidden align-[-13px] dark:inline-flex"
+                className="logo hidden align-[-9px] dark:inline-flex"
                 alt="Hybridger logo"
-                width={50}
-                height={77}
+                width={93}
+                height={60}
                 src={darkModeLogo}
             />
         </>
